5a27956f4e20ff3c1a6439deecc2086c30194f4f
HoonC-corgi <tgh7544@naver.com>
2022-11-16 12:36:50 +0900

    "DoWhile" loop practice

---
+++ src/main/java/LoopDoWhile/Main.java
@@ -0,0 +1,12 @@
+package LoopDoWhile;
+
+public class Main {
+    public static void main(String[] args) {
+        char c = 'a';
+
+        do{
+            System.out.print(c + " ");
+            c += 1;
+        }while(c<='z');
+    }
+}
