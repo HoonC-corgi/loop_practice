b456c51b8fbf87c21c319bdf65402e2798ab722c
HoonC-corgi <tgh7544@naver.com>
2022-11-16 12:32:56 +0900

    "while" loop practice

--- src/main/java/LoopWhile/Main.java
+++ src/main/java/LoopWhile/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("exit을 입력시 종료");
+
         while(true) {   // 무한반복, 0 또는 1 사용 x 반드시 불린타입
             System.out.print(">> ");
             String text = scanner.nextLine();   // string 변수에 스캐너로 입력받음
